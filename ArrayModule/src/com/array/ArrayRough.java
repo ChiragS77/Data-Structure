1112b6af7722f40abb2da82327f04687afdf6f27
ChiragS77 <deshmukhchirag178@gmail.com>
2024-06-30 21:00:01 +0530

    array code changes

--- ArrayModule/src/com/array/ArrayRough.java
+++ ArrayModule/src/com/array/ArrayRough.java
@@ -31,12 +31,80 @@ public class ArrayRough {
 		
 	}
 	
+	public static void kadansAlgo(int arr[]) {
+		
+		int currSum = 0;
+		int maxSum = Integer.MIN_VALUE;
+		
+		for(int i= 0; i<arr.length; i++) {
+			currSum += arr[i];
+			if(currSum < 0) {
+				currSum = 0;
+			}
+			if(maxSum < currSum) {
+				maxSum = currSum;
+			}
+		}
+		
+		System.out.println("Maximum sum is: "+maxSum);
+	}
+	
+	static int count_greater(int arr[], int n)
+	{
+	    int min = Integer.MAX_VALUE;
+	    int counter = 0;
+	 
+	    // Comparing the given element
+	    // with minimum element till
+	    // occurred till now.
+	    for (int i = n - 1; i >= 0; i--)
+	    {
+	        if (arr[i] > min)
+	        {
+	            counter++;
+	        }
+	 
+	        // Updating the min variable
+	        if (arr[i] <= min)
+	        {
+	            min = arr[i];
+	        }
+	    }
+	    return counter;
+	}
+	//two sum
+	public static int twoSum(int arr[], int s) {
+		int start = 0;
+		int end = arr.length -1;
+		
+		while(start<end) {
+			if(arr[start]+arr[end]==s) {
+				return 1;
+			}
+			if(arr[start]+arr[end]>s) {
+				end--;
+			}
+			if(arr[start] + arr[end]< s) {
+				start++;
+			}
+			
+		}
+		return -1;
+	}
 	
 	public static void main(String[] args) {
 		int arr[] = { 1,-2,6,-1,3 };
 		int sum = 22;
-		printMaxSum(arr); 
+//		printMaxSum(arr); 
+//		kadansAlgo(arr);
+		
+//		int a[] = {3, 9, 4, 6, 7, 5 };
+//	int c =	count_greater(a,a.length);
+//	System.out.println(c);
 
+		int a[]= {1,2,3,4,5};
+		int x =twoSum(a,4); 
+		System.out.println(x);
 	}
 
 
